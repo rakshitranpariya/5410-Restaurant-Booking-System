b367203aad2f306b35aa299e3b7ca14e2419dcaa
boon <aaron25rony@gmail.com>
2023-10-25 20:51:19 -0300

    updated the lambda function that sends notification when there is a change in the menu in dynamoDB

--- lambda/menu_update_cust_notif.py
+++ lambda/menu_update_cust_notif.py
@@ -1,11 +1,25 @@
+import json
 import boto3
 
-topic_arn = "arn:aws:sns:us-east-1:315128346896:menu-update" 
-# update to actual SNS topic
-
-def send_sns(message, subject):
+def lambda_handler(event, context):
     try:
+        event_name = event["Records"][0]["eventName"]
+        
+        if event_name == "INSERT":
+            print("IM INSERT")
+        else if event_name == "MODIFY":
+            print("IM moify")
+        else if event_name == "REMOVE":
+            print("IM remove")
+            
+        # add proper message and subject based on the event_type: insert, modify, remove
+        topic_arn = "arn:aws:sns:us-east-1:315128346896:menu-item-update"
+        
         client = boto3.client("sns")
+        # sample message and subject
+        message = "Mutton Biryani price is reduced by $1.49."
+        subject = "There's a change in our menu !!"
+        
         result = client.publish(TopicArn=topic_arn, Message=message, Subject=subject)
         if result['ResponseMetadata']['HTTPStatusCode'] == 200:
             print(result)
@@ -13,14 +27,4 @@ def send_sns(message, subject):
             return True
     except Exception as e:
         print("Error occured while publish notifications and error is : ", e)
-        return True
-
-def lambda_handler(event, context):
-        subject = "test sub"
-        message = "hi"
-        SNSResult = send_sns(message, subject)
-        if SNSResult :
-            print("Notification Sent..") 
-            return SNSResult
-        else:
-            return False
+        return false
