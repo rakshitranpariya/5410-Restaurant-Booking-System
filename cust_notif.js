0ec3ea3a1e68d3b1200626d352e97d7e72ada4ed
boon <aaron25rony@gmail.com>
2023-10-25 19:33:29 -0300

    subscribe api for AWS SNS menu item update topic done

---
+++ cust_notif.js
@@ -0,0 +1,39 @@
+const express = require('express');
+require('dotenv').config();
+const app = express();
+const AWS = require('aws-sdk');
+const port = 3000;
+
+app.use(express.json())
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.post('/subscribe', (req, res) => {
+
+    AWS.config.update({
+        accessKeyId: process.env.aws_access_key_id,
+        secretAccessKey: process.env.aws_secret_access_key,
+        region: process.env.aws_sns_region,
+        sessionToken: process.env.aws_session_token
+    })
+
+    const sns = new AWS.SNS();
+    
+    const params = {
+        Protocol: 'email',
+        TopicArn: process.env.aws_sns_menu_item_update_topic_arn,
+        Endpoint: req.body.email
+    }
+
+    sns.subscribe(params, (err, data) => {
+        if (err) console.error(err);
+        res.send(data);
+    });
+
+});
