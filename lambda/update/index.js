af2b1abe32cdcc7a8c4fcc86bcd5b194370a3b0a
Jinal Dave <jn270099@dal.ca>
2023-10-27 14:48:40 -0300

    5.1.3 Book, edit, delete, view a reservation

---
+++ lambda/update/index.js
@@ -0,0 +1,104 @@
+const admin = require("firebase-admin");
+const serviceAccount = require("./serverless.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const db = admin.firestore();
+
+exports.handler = async (event, context) => {
+  try {
+    // Check if a valid document ID is provided in the path parameters
+    const bodyCheck = event.body;	
+	 
+	 
+    if (!bodyCheck) {
+      return {
+        statusCode: 400,
+		headers: {
+            "Access-Control-Allow-Headers" : "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "*"
+        },
+        body: JSON.stringify({
+          error: "Bad Request",
+          message: "Params missing",
+        })
+       
+      };
+    }
+	
+	const docIdCheck = JSON.parse(event.body).pathParameters;
+	 
+    if (!docIdCheck) {
+      return {
+        statusCode: 400,
+		headers: {
+            "Access-Control-Allow-Headers" : "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "*"
+        },
+        body: JSON.stringify({
+          error: "Bad Request",
+          message: "Body is missing in the parameters",
+        })
+      };
+    }
+
+const docId = JSON.parse(event.body).pathParameters.id;
+    
+
+    // Parse the request body as JSON
+    let updatedData;
+    try {
+      updatedData = JSON.parse(event.body).body;
+    } catch (parseError) {
+      console.error("Error parsing JSON:", parseError);
+      return {
+        statusCode: 400,
+		headers: {
+            "Access-Control-Allow-Headers" : "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "*"
+        },
+        body: JSON.stringify({
+          error: "Bad Request",
+          message: "Invalid JSON format in the request body",
+        })
+      };
+    }
+
+    // Perform the Firestore update operation
+    await db.collection("reservations").doc(docId).update(updatedData);
+
+    const response = {
+      statusCode: 200,
+	  headers: {
+            "Access-Control-Allow-Headers" : "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "*"
+        },
+      body: JSON.stringify({ message: "Document updated successfully" })
+    };
+
+    return response;
+  } catch (error) {
+    console.error("Error updating document:", error);
+
+    const errorResponse = {
+      statusCode: 500,
+	  headers: {
+            "Access-Control-Allow-Headers" : "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "*"
+        },
+      body: JSON.stringify({
+        error: "Internal Server Error",
+        message: error.message,
+      })
+    };
+
+    return errorResponse;
+  }
+};
