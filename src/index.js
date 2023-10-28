29e7850047f700e7f31375c784df1c95510dd2c2
bhunsadiya <mh207556@dal.ca>
2023-10-27 22:01:41 -0300

    added chatbot library

--- src/index.js
+++ src/index.js
@@ -3,9 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Kommunicate from "@kommunicate/kommunicate-chatbot-plugin";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
+
+Kommunicate.init("2b6c021219d0e7ce14f57aba4bcc1cca6", {
+    automaticChatOpenOnNavigation: true,
+    popupWidget: true
+  });
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
