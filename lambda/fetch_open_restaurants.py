6bbedea63d8ccd328db1e6be36dd8b3c86b89d51
boon <aaron25rony@gmail.com>
2023-11-18 23:31:03 -0400

    restaurants notifications completed

---
+++ lambda/fetch_open_restaurants.py
@@ -0,0 +1,49 @@
+import json
+import requests
+import boto3
+from datetime import datetime
+
+def lambda_handler(event, context):
+    api_url = 'https://vzgth5nw0m.execute-api.us-east-1.amazonaws.com/prod/5410-project-getretaurantListOnCurrentTime'
+    current_time = datetime.now().strftime("%H:%M:%S")
+
+    print(current_time)
+    payload = {
+        "time": "21:00:00" 
+            #current_time --> change it back to current_time in prod
+    }
+    
+    try:
+        # Make a POST request to the API
+        response = requests.post(api_url, json=payload)
+        response_data = response.json()
+        
+        print("got response from get-restaurants api")
+        print(response_data)
+        
+        sqs = boto3.client("sqs")
+        
+        if response.status_code == 200:
+            
+            list_of_restaurants_json_string = json.dumps(response_data)
+
+            sqs_response = sqs.send_message(
+                QueueUrl = "https://sqs.us-east-1.amazonaws.com/315128346896/open_restaurants_list_queue",
+                MessageBody = list_of_restaurants_json_string)
+            return {
+                'statusCode': sqs_response["ResponseMetadata"]["HTTPStatusCode"],
+                'body': json.dumps(sqs_response["ResponseMetadata"])
+            }
+
+    except Exception as e:
+        # Handle any exceptions
+        return {
+            'statusCode': 500,
+            'body': f'Error: {str(e)}'
+        }
+    
+    return {
+        'statusCode': 200,
+        'body': "Something went wrong"
+    }    
+    
