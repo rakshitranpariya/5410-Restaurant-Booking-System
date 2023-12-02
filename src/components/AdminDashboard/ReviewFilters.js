5d2a16559970f7520f9f471a15335e5de53c6494
bhunsadiya <mh207556@dal.ca>
2023-12-02 19:41:11 -0400

    changed url for looker studio

--- src/components/AdminDashboard/ReviewFilters.js
+++ src/components/AdminDashboard/ReviewFilters.js
@@ -6,7 +6,7 @@ const ReviewFilters = () => {
           title="Food Order Report"
           width="100%"
           height="1000"
-          src="https://lookerstudio.google.com/embed/reporting/051a3f6d-b42a-4249-9c56-1d95ec1f379e/page/tEnnC"
+          src="https://lookerstudio.google.com/embed/reporting/2f0891c8-b993-4877-8b10-192773ffb4ed/page/tEnnC"
           style={{ border: 0, marginTop: '20px' }}
           allowFullScreen
         ></iframe>
