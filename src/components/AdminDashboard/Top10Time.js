a2e4a33e36ef36967d25a0ad1b8f2b803a5d3300
Smith Chokshi <sm714486@dal.ca>
2023-12-02 21:12:52 -0400

    implemented the graph for top 10 time for most ordered food

---
+++ src/components/AdminDashboard/Top10Time.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const TopTime = () => {
+
+
+    return (
+        <iframe
+            title="Top 10 Time"
+            width="100%"
+            height="1000"
+            src="https://lookerstudio.google.com/embed/reporting/9d253232-1ced-47cf-8d20-f3b0cd74156b/page/6zXD"
+            style={{ border: 0, marginTop: '20px' }}
+            allowFullScreen
+        ></iframe>
+    );
+};
+
+export default TopTime;
