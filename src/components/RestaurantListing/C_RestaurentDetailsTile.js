2a45c03a3ab7606a516e9187820f8d7059d27919
Rakshit <rk371915@dal.ca>
2023-12-04 08:08:40 -0400

    Retrieved the gone changes

--- src/components/RestaurantListing/C_RestaurentDetailsTile.js
+++ src/components/RestaurantListing/C_RestaurentDetailsTile.js
@@ -1,7 +1,7 @@
-import React, { useCallback } from "react";
-import { useNavigate } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./C_RestaurentDetailsTile.css";
+import React, { useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './C_RestaurentDetailsTile.css';
 
 const RestaurantTile = ({
   title,
@@ -17,53 +17,65 @@ const RestaurantTile = ({
 
   const handleViewMenu = useCallback(() => {
     if (restaurantId) {
-      restaurantId = "123456";
-      console.log("Navigating to menu with restaurantId:", restaurantId);
+      restaurantId = '123456';
+      console.log('Navigating to menu with restaurantId:', restaurantId);
       navigate(`/menu/${restaurantId}`);
     } else {
-      console.error("restaurantId is undefined");
+      console.error('restaurantId is undefined');
     }
   }, [navigate, restaurantId]);
 
   return (
-    <div className="card mb-4" style={{ fontFamily: "Open Sans, sans-serif" }}>
+    <div className={`offer-item card mb-4`} style={{ fontFamily: 'Open Sans, sans-serif' }}>
       <div className="card-body">
-        <h2 className="card-title mb-3">{title}</h2>
-        <p className="card-text mb-2">City: {city}</p>
-        <div className="mb-2">
-          <p className="card-text">
-            <strong>Opening Time:</strong> {startTime}
-          </p>
+        <div className="offer-item-content text-center">
+          <div className="row align-items-center">
+            <div className="col-md-3 mb-2">
+              <div className="card-title">Title: {title}</div>
+            </div>
+            <div className="col-md-3 mb-2">
+              <div className="card-text">City: {city}</div>
+            </div>
+            <div className="col-md-3 mb-2">
+              <div className="card-text">
+                <strong>Opening Time:</strong> {startTime}
+              </div>
+            </div>
+            <div className="col-md-3 mb-2">
+              <div className="card-text">
+                <strong>Closing Time:</strong> {endTime}
+              </div>
+            </div>
+            <div className="col-md-3 mb-2">
+              <div className="card-text">
+                <strong>Email:</strong> {email}
+              </div>
+            </div>
+            <div className="col-md-3 mb-2">
+              <div className="card-text">
+                <strong>Status:</strong> {currentStatus}
+              </div>
+            </div>
+            <div className="col-md-3 mb-2">
+              {availability === 'available' ? (
+                <div className="card-text">
+                  <strong className="text-success">Available</strong>
+                </div>
+              ) : (
+                <div className="card-text">
+                  <strong className="text-danger">
+                    This restaurant is not available for booking.
+                  </strong>
+                </div>
+              )}
+            </div>
+            <div className="col-md-3 text-md-end">
+              <button className="btn btn-primary" onClick={handleViewMenu}>
+                View Menu
+              </button>
+            </div>
+          </div>
         </div>
-        <div className="mb-3">
-          <p className="card-text">
-            <strong>Closing Time:</strong> {endTime}
-          </p>
-        </div>
-        <p className="card-text">
-          <strong>Email:</strong> {email}
-        </p>
-        <p className="card-text">
-          <strong>Status:</strong> {currentStatus}
-        </p>
-        {availability === "available" ? (
-          <p className="card-text">
-            <strong className="text-success">Available</strong>
-          </p>
-        ) : (
-          <p className="card-text">
-            <strong className="text-danger">
-              This restaurant is not available for booking.
-            </strong>
-          </p>
-        )}
-        <button
-          type="button"
-          className="btn btn-primary"
-          onClick={handleViewMenu}
-        >
-          View Menu
-        </button>
       </div>
     </div>
   );
--- src/components/RestaurantListing/P_RestaurantListingPage.js
+++ src/components/RestaurantListing/P_RestaurantListingPage.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from "react";
-import RestaurantTile from "./C_RestaurentDetailsTile";
-import { useNavigate } from "react-router-dom";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import RestaurantTile from './C_RestaurentDetailsTile';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 const RestaurantListingPage = () => {
   const [restaurantData, setRestaurantData] = useState([]);
@@ -10,54 +10,47 @@ const RestaurantListingPage = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        console.log("Fetching data...");
+        console.log('Fetching data...');
 
-        const response = await axios.get(
-          "https://vzgth5nw0m.execute-api.us-east-1.amazonaws.com/prod/getRestaurantData"
+        const response = await axios.post(
+          'https://vzgth5nw0m.execute-api.us-east-1.amazonaws.com/prod/getRestaurantData'
         );
 
         const data = response.data.body;
         console.log(data);
         setRestaurantData(JSON.parse(data));
       } catch (error) {
-        console.error("Error:", error.message);
+        console.error('Error:', error.message);
       }
     };
-    //.
     fetchData();
-  }, []); // Empty dependency array to fetch data only on component mount
+  }, []);
 
   const handleReservationClick = () => {
-    // Navigate to "/restaurants"
-    navigate("/restaurants");
+    navigate('/restaurants');
   };
 
   return (
-    <div>
-      <h1 className="m-5">Restaurant List</h1>
-      <div className="container">
-        <div className="row">
-          {restaurantData.map((restaurant) => (
-            <div key={restaurant.id} className="col-md-4">
-              <RestaurantTile
-                restaurantId={restaurant.id}
-                email={restaurant.email}
-                title={restaurant.name}
-                city={restaurant.city}
-                startTime={restaurant.openingHours}
-                endTime={restaurant.closingHours}
-                currentStatus={restaurant.currentStatus}
-                availability={restaurant.availability}
-              />
-            </div>
-          ))}
-        </div>
+    <div className="my-5 mx-auto" style={{ maxWidth: '800px' }}>
+      <h1 className="text-center mb-5">Restaurant List</h1>
+      <div className="row">
+        {restaurantData.map(restaurant => (
+          <div key={restaurant.id} className="col-md-12 mb-4">
+            <RestaurantTile
+              restaurantId={restaurant.id}
+              email={restaurant.email}
+              title={restaurant.name}
+              city={restaurant.city}
+              startTime={restaurant.openingHours}
+              endTime={restaurant.closingHours}
+              currentStatus={restaurant.currentStatus}
+              availability={restaurant.availability}
+            />
+          </div>
+        ))}
       </div>
       <div className="text-center mt-4">
-        <button
-          className="btn btn-primary btn-lg"
-          onClick={handleReservationClick}
-        >
+        <button className="btn btn-primary btn-lg" onClick={handleReservationClick}>
           Make a Reservation
         </button>
       </div>
