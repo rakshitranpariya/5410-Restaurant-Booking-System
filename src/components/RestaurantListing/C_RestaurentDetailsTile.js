81d279cbb3c7f01074ba4516806108502b630b9b
Rakshit <rk371915@dal.ca>
2023-12-04 08:20:15 -0400

    updating the restuarantListing page

--- src/components/RestaurantListing/C_RestaurentDetailsTile.js
+++ src/components/RestaurantListing/C_RestaurentDetailsTile.js
@@ -25,54 +25,57 @@ const RestaurantTile = ({
     }
   }, [navigate, restaurantId]);
 
+  // Convert time to AM/PM format
+  const formatTime = time => {
+    const date = new Date(`2000-01-01T${time}`);
+    return date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+  };
+
   return (
     <div className={`offer-item card mb-4`} style={{ fontFamily: 'Open Sans, sans-serif' }}>
       <div className="card-body">
-        <div className="offer-item-content text-center">
-          <div className="row align-items-center">
-            <div className="col-md-3 mb-2">
-              <div className="card-title">Title: {title}</div>
-            </div>
-            <div className="col-md-3 mb-2">
-              <div className="card-text">City: {city}</div>
-            </div>
-            <div className="col-md-3 mb-2">
-              <div className="card-text">
-                <strong>Opening Time:</strong> {startTime}
-              </div>
-            </div>
-            <div className="col-md-3 mb-2">
-              <div className="card-text">
-                <strong>Closing Time:</strong> {endTime}
-              </div>
-            </div>
-            <div className="col-md-3 mb-2">
-              <div className="card-text">
-                <strong>Email:</strong> {email}
-              </div>
-            </div>
-            <div className="col-md-3 mb-2">
-              <div className="card-text">
-                <strong>Status:</strong> {currentStatus}
-              </div>
-            </div>
-            <div className="col-md-3 mb-2">
-              {availability === 'available' ? (
+        <div className="offer-item-content">
+          <div className="row">
+            <div className="col-md-6">
+              <div className="card-title h5">{title}</div>
+              <div className="col-md-12 mb-2">
                 <div className="card-text">
-                  <strong className="text-success">Available</strong>
+                  <strong>Status:</strong> {currentStatus}
                 </div>
-              ) : (
-                <div className="card-text">
-                  <strong className="text-danger">
-                    This restaurant is not available for booking.
-                  </strong>
-                </div>
-              )}
+              </div>
+              <div className="card-text h6">City: {city}</div>
             </div>
-            <div className="col-md-3 text-md-end">
-              <button className="btn btn-primary" onClick={handleViewMenu}>
-                View Menu
-              </button>
+            <div className="col-md-6">
+              <div className="row">
+                <div className="col-md-6 mb-2">
+                  <div className="card-text">
+                    <strong>Opening Time:</strong> {formatTime(startTime)}
+                  </div>
+                </div>
+                <div className="col-md-6 mb-2">
+                  <div className="card-text">
+                    <strong>Closing Time:</strong> {formatTime(endTime)}
+                  </div>
+                </div>
+                <div className="col-md-12 mb-2">
+                  {availability === 'available' ? (
+                    <div className="card-text">
+                      <strong className="text-success">Available</strong>
+                    </div>
+                  ) : (
+                    <div className="card-text">
+                      <strong className="text-danger">
+                        This restaurant is not available for booking.
+                      </strong>
+                    </div>
+                  )}
+                </div>
+                <div className="col-md-12 text-md-end">
+                  <button className="btn btn-primary" onClick={handleViewMenu}>
+                    View Menu
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
